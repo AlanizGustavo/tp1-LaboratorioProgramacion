a0ec9e54eee836bad653e727725c4f1f2dafd437
Leonel Bustamante <aleobustamante@gmail.com>
2022-09-15 12:28:42 -0300

    Se crea un ejemplo para scheduleWithFixedDelay

--- Concurrente/scheduleExecutorService.java
+++ Concurrente/scheduleExecutorService.java
@@ -10,46 +10,63 @@ public class scheduleExecutorService {
         final int horasDelDia = 24;
         int cantDias, horas, procesadores, i;
         Pastilla pastilla;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Ingrese el nombre de su medicamento: ");
-        pastilla = new Pastilla(in.nextLine());
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Ingrese el nombre de su medicamento: ");
+            pastilla = new Pastilla(in.nextLine());
 
-        System.out.print("Ingrese cada cuantas horas debe tomarlo: ");
-        horas = in.nextInt();
+            System.out.print("Ingrese cada cuantas horas debe tomarlo: ");
+            horas = in.nextInt();
 
-        System.out.print("Ingrese cuantos días debe tomarlo: ");
-        cantDias = in.nextInt();
-
+            System.out.print("Ingrese cuantos días debe tomarlo: ");
+            cantDias = in.nextInt();
+        }
         procesadores = Runtime.getRuntime().availableProcessors();
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(procesadores);
 
-        // Forma 1
+        // Forma 1 - Se ejecutan todas las tareas juntas pero se el delay hace que se
+        // vean en orden
+        int duracion = horasDelDia * cantDias;
         System.out.println("Forma 1");
-        for (i = (cantDias * horasDelDia); i >= 0; i--) {
-            scheduledExecutorService.schedule(new Reloj(), (horasDelDia * cantDias) - i, TimeUnit.SECONDS);
+        for (i = duracion; i >= 0; i--) {
+            scheduledExecutorService.schedule(new Reloj(), duracion - i, TimeUnit.SECONDS);
             if (i % horas == 0) {
-                scheduledExecutorService.schedule(new Recordatorio(pastilla), (horasDelDia * cantDias) - i,
-                        TimeUnit.SECONDS);
+                scheduledExecutorService.schedule(new Recordatorio(pastilla), duracion - i, TimeUnit.SECONDS);
             }
         }
         try {
-            Thread.sleep(cantDias * horasDelDia * 1000);
+            // Se espera a que terminen todas las tareas
+            Thread.sleep(duracion * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         scheduledExecutorService.shutdown();
 
-        // Forma 2
+        // Forma 2 - Todas las tareas se ejecutan al mismo tiempo, pero luego de 1
+        // segundo se ejecuta la tarea siguiente
         System.out.println("Forma 2");
         ScheduledExecutorService scheduledExecutorService2 = Executors.newScheduledThreadPool(procesadores);
         scheduledExecutorService2.scheduleAtFixedRate(new Reloj(), 0, 1, TimeUnit.SECONDS);
         scheduledExecutorService2.scheduleAtFixedRate(new Recordatorio(pastilla), 0, horas, TimeUnit.SECONDS);
         try {
-            Thread.sleep(cantDias * horasDelDia * 1000);
+            // Se espera a que terminen todas las tareas
+            Thread.sleep(duracion * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         scheduledExecutorService2.shutdown();
+
+        // Forma 3 - Se ejecuta la proxima tarea cuando termina la anterior
+        System.out.println("Forma 3");
+        ScheduledExecutorService scheduledExecutorService3 = Executors.newScheduledThreadPool(procesadores);
+        scheduledExecutorService3.scheduleWithFixedDelay(new Reloj(), 0, 1, TimeUnit.SECONDS);
+        scheduledExecutorService3.scheduleWithFixedDelay(new Recordatorio(pastilla), 0, horas, TimeUnit.SECONDS);
+        try {
+            // Se espera a que terminen todas las tareas
+            Thread.sleep(duracion * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        scheduledExecutorService3.shutdown();
     }
 
 }
